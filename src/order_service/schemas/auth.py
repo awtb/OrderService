1d4b94f1ec08dcfe5d26fce2616a8f66be747519
Ilyas Qalandarzoda <ilyas.qalandarzoda@icloud.com>
2025-12-21 12:28:48 +0000

    refactor(api): Use pydantic-way to for email-normalization.

--- src/order_service/schemas/auth.py
+++ src/order_service/schemas/auth.py
@@ -2,6 +2,7 @@ from order_service.dto.auth import RegistrationRequestDTO
 from order_service.schemas.base import BaseSchema
 from pydantic import EmailStr
 from pydantic import Field
+from pydantic import field_validator
 
 
 class TokenSchema(BaseSchema):
@@ -14,9 +15,14 @@ class RegistrationRequestSchema(BaseSchema):
     email: EmailStr
     password: str = Field(min_length=8, max_length=128)
 
+    @field_validator("email", mode="before")
+    @classmethod
+    def normalize_email(cls, value: str) -> str:
+        return value.lower()
+
     def to_dto(self) -> RegistrationRequestDTO:
         return RegistrationRequestDTO(
-            email=str(self.email).lower(),
+            email=str(self.email),
             password=self.password,
         )
 
