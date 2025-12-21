db62fbd472418ccf5a90967637231b99e8420d81
Ilyas Qalandarzoda <ilyas.qalandarzoda@icloud.com>
2025-12-21 11:54:37 +0000

    feat(api): Add `Page` schema.

--- src/order_service/schemas/base.py
+++ src/order_service/schemas/base.py
@@ -1,6 +1,20 @@
+from typing import Generic
+from typing import TypeVar
+
 from pydantic import BaseModel
 from pydantic import ConfigDict
 
 
 class BaseSchema(BaseModel):
     model_config = ConfigDict(from_attributes=True)
+
+
+T = TypeVar("T", bound=BaseModel)
+
+
+class Page(Generic[T], BaseSchema):
+    page_size: int
+    total_pages: int
+    total_items: int
+    page: int
+    items: list[T]
